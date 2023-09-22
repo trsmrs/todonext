be5c45db44b4a3bd854ea1944e4c92d3e7f7cfa3
Tiago <trsminfo@gmail.com>
2023-09-22 12:40:35 -0300

    finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import styles from '../styles/home.module.css'
 import Image from 'next/image'
 
 import todoImg from '../../public/assets/todo.jpg'
+import { db } from '../services/firebaseConnection'
+import {
+ collection,
+ getDocs
+} from 'firebase/firestore'
 
-export default function Home() {
+interface HomeProps{
+  posts: number;
+  comments: number;
+}
+
+export default function Home({posts, comments}: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -22,13 +33,30 @@ export default function Home() {
 
           <div className={styles.infoContent}>
             <section className={styles.box}>
-              <span>+10 posts</span>
+              <span>+ {posts} Posts...</span>
             </section>
             <section className={styles.box}>
-              <span>+100 Comentários</span>
+              <span>+ {comments} Comentários...</span>
             </section>
           </div>
       </main>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () =>{
+
+  const commentRef = collection(db, "comments")
+  const postRef = collection(db, "tarefas")
+
+  const commentSnapshot = await getDocs(commentRef)
+  const postSnapshot = await getDocs(postRef)
+
+  return{
+    props:{
+      posts: postSnapshot?.size,
+      comments: commentSnapshot?.size
+    },
+    revalidate: 60
+  }
 }
